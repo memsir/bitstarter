4b969829066cba0ad89ab1beeb8966292dd480d2
memsir <memsir@gmail.com>
2013-07-23 21:05:31 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   car fs = require("fs");
+   var buffer = new buffer();
+
+   response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
